4737561e2aaa26579eef71dc1a017f76abf28c48
pcoder <purple.coder@yahoo.co.uk>
2013-08-04 21:17:21 +0200

    Updated version of compiler.php

--- compiler.php
+++ compiler.php
@@ -10,7 +10,7 @@ $wiselibUUID=$my_data["wiselibUUID"];
 $makeTarget=$my_data["build"];
 
 foreach ($my_data['files'] as $key => $value){
-        file_put_contents($basepath."app.cpp", $value['content'], LOCK_EX);
+        file_put_contents($basepath."app.cpp", html_entity_decode($value['content']), LOCK_EX);
 }
 
 chdir( $basepath );
@@ -31,14 +31,20 @@ fclose($handle);
 $response["success"]=$retval===0;
 $response["size"]=$size;
 $response["message"]=implode("<br/>",$retstr);
-$response["output"]=base64_encode($contents);
+//file_put_contents($basepath."app.before",$contents), LOCK_EX);
 
+$tmpfname = tempnam($basepath . "out/isense/tmp/", "app_");
 
-print_r($response);
+$handle = fopen($basepath. "out/isense/tmp/" . basename($tmpfname), "w");
+fwrite($handle, $contents);
+fclose($handle);
+
+$response["output"]= basename($tmpfname);
+
+print_r(json_encode($response));
 
 //unlink($basepath."app.cpp");
-unlink($basepath.$isense_sub_path);
+//unlink($basepath.$isense_sub_path);
 //print_r($my_data['files']);
 
 ?>
-
