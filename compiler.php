f91f24773ac172ae2ad89ace58478e87f20569ba
pcoder <purple.coder@yahoo.co.uk>
2013-08-10 12:51:40 +0200

    Added clauses for different platforms.

--- compiler.php
+++ compiler.php
@@ -7,7 +7,19 @@ $basepath="/compiler/app/";
 
 $username=$my_data["username"];
 $wiselibUUID=$my_data["wiselibUUID"];
-$makeTarget=$my_data["build"];
+$makeTarget=trim(strtolower($my_data["build"]));
+
+switch($makeTarget){
+	case "isense":
+		$makeTarget = "isense";
+		break;
+	case "isense5148":
+		$makeTarget = "isense.5148";
+		break;
+	case "shawn":
+		$makeTarget = "shawn";
+		break;
+}
 
 foreach ($my_data['files'] as $key => $value){
         file_put_contents($basepath."app.cpp", html_entity_decode($value['content']), LOCK_EX);
